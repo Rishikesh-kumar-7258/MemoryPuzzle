4538fd2ce93129cee20819cea713279bf5ba23c6
Rishikesh-kumar-7258 <rishi7258prince@gmail.com>
2021-09-18 22:26:58 +0530

    made fliper class

---
+++ classes/fliper.py
@@ -0,0 +1,38 @@
+import pygame
+
+from functions import Write
+
+class Fliper:
+
+    def __init__(self, x=0, y=0, color=(255,255,255), background=(0,0,0), width=50, height=50, screen=None, text=1):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.background = background
+        self.width = width
+        self.height = height
+        self.screen = screen
+        self.text = text
+
+        self.isflip = False
+
+        box = pygame.draw.rect(self.screen, self.background, [self.x, self.y, self.width, self.height])
+
+    def render(self) : 
+        box = pygame.draw.rect(self.screen, self.background, [self.x, self.y, self.width, self.height])
+        if self.isflip : Write(self.screen, str(self.text), self.x + self.width // 2, self.y + self.height // 2, self.color, self.background, size=int(0.70*self.height), center=True)
+
+    def update(self):
+
+        if self.clicked() : self.flip()
+        self.render()
+
+    def clicked(self):
+        x,y = pygame.mouse.get_pos()
+        a, b, c = pygame.mouse.get_pressed()
+
+        return a and x >= self.x and x <= self.x + self.width and y >= self.y and y <= self.y + self.height
+    
+    def flip(self):
+
+        self.isflip = True
--- states/playstate.py
+++ states/playstate.py
@@ -2,23 +2,22 @@ import pygame
 
 from states.basestate import Base
 
-from classes.changer import Changer
+from classes.fliper import Fliper
+
 from functions import *
 
 class Play(Base):
 
     def __init__(self):
         
-        self.changer = Changer()
-        
+        # self.fliper = Fliper()    
+        pass
 
     def render(self) :
-        Write(self.screen, "Play", self.WINDOW_WIDTH // 2, self.WINDOW_HEIGHT // 2, size=110, center=True)
-        self.changer.render()
-
+        self.fliper.render()
     def update(self, params) :
 
-        self.changer.update(params)
+        self.fliper.update()
         self.render()
 
     def enter(self, **params):
@@ -26,6 +25,4 @@ class Play(Base):
         self.screen = params['screen']
         self.WINDOW_WIDTH = params['windowWidth']
         self.WINDOW_HEIGHT = params['windowHeight']
-        self.changer = Changer(x=self.WINDOW_WIDTH // 2, y=self.WINDOW_HEIGHT//2 + 200, screen=self.screen)
-        self.changer.actions = ["play"]
-        self.changer.gameState = self.GAME_STATE
+        self.fliper = Fliper(screen=self.screen, background=(255, 0, 0))
