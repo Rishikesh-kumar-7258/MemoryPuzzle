4fbf2542ec98f80cbe2839fddc6999f9f0de7a45
Rishikesh-kumar-7258 <rishi7258prince@gmail.com>
2021-09-27 23:14:15 +0530

    changed mouse click to keyboard click

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ pygame.display.set_caption("Memory Game")
 COLORS = {
     "white" : (255, 255, 255),
     "black" : (0, 0, 0),
-    'green' : (0, 255, 0)
+    'green' : (0, 255, 0),
 }
 
 STATES = {
--- states/playstate.py
+++ states/playstate.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.constants import KEYDOWN, SRCALPHA
 
 from states.basestate import Base
 
@@ -14,30 +15,45 @@ class Play(Base):
         self.blockCount = 3
         self.flipers = []
 
-        self.selected = None
+        self.selected = 0
+        self.current = None
+        self.previous = None
 
     def render(self) :
         self.playspace = pygame.draw.rect(self.screen, (50, 0, 0), [190, 50, 600, 500])
         for fliper in self.flipers:
             fliper.render()
+        
+        selectbox = self.flipers[self.selected]
+        tempbox = pygame.Surface((selectbox.width, selectbox.height), SRCALPHA)
+        tempbox.set_colorkey(self.colors["black"])
+        tempbox.set_alpha(120)
+        pygame.draw.rect(tempbox, self.colors["green"], tempbox.get_rect(), 10)
+
+        self.screen.blit(tempbox, (selectbox.x, selectbox.y))
+
 
     def update(self, params) :
 
-        for fliper in self.flipers:
-
-            if fliper.clicked():
+        for event in params:
+            if event.type == KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    self.selected = self.selected - 1 if (self.selected > 0) else self.selected
+                if event.key == pygame.K_RIGHT:
+                    self.selected = self.selected + 1 if self.selected < self.blockCount*4 - 1 else self.selected
+                if event.key == pygame.K_UP:
+                    self.selected = self.selected - 4 if self.selected > 3 else self.selected
+                if event.key == pygame.K_DOWN:
+                    self.selected = self.selected + 4 if self.selected < (self.blockCount-1)*4 else self.selected
+                if event.key == pygame.K_SPACE:
+                    self.current = self.flipers[self.selected]
+                    self.current.isflip = True
                 print(self.selected)
-                if self.selected == None:
-                    self.selected = fliper
-                    fliper.isflip = True
-                else : 
-                    if self.selected == fliper: fliper.isflip = True
-                    else : self.selected.isflip = False
-                    self.selected = None
                 
         self.render()
 
     def enter(self, **params):
+        self.colors = params["colors"]
         self.GAME_STATE = params['gameState']
         self.screen = params['screen']
         self.WINDOW_WIDTH = params['windowWidth']
